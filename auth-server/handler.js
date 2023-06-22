9ab148e0f91112c85df837bd7e5f8a4316e02a9d
Darkpfeffer <darkpfeffer97@gmail.com>
2023-06-22 21:50:25 +0200

    trying to solve authentication problem

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -14,14 +14,14 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://darkpfeffer.github.io/meet-app/"],
+  redirect_uri: ["https://darkpfeffer.github.io/meet-app/"],
   javascript_origins: ["https://darkpfeffer.github.io", "http://localhost:3000"],
 };
-const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
+const { client_secret, client_id, redirect_uri, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
   client_id,
   client_secret,
-  redirect_uris[0]
+  redirect_uri[0]
 );
 
 //create and export the getAuthURL function
