1af18fa7ea6f1b997a5966d8838765b708f5b59b
Darkpfeffer <121576620+Darkpfeffer@users.noreply.github.com>
2023-07-09 20:26:14 +0200

    Integration testing for Feature 1 Scenario 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
+//import
+import { useState, useEffect } from 'react';
+
 //import components
 import { CitySearch } from './components/CitySearch/CitySearch';
 import { EventList } from './components/EventList/EventList';
 import { NumberOfEvents } from './components/NumberOfEvents/NumberOfEvents';
 
+//import functions
+import { getEvents } from './api';
+
 //import self css
 import './App.css';
 
 export const App = () => {
+  const [events, setEvents] = useState([]);
+  //current number of events
+  const [currentNOE, setCurrentNOE] = useState(32);
+
+  const fetchData = async() => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className='App'>
-      <EventList />
+      <EventList events={events} />
       <CitySearch />
-      <NumberOfEvents />
+      <NumberOfEvents/>
     </div>
   )
 }
--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,7 +1,8 @@
 //import
-import { render } from '@testing-library/react';
+import { render, within, waitFor } from '@testing-library/react';
 
 //import components
+import { App } from '../App';
 import { EventList } from '../components/EventList/EventList';
 
 //import functions
@@ -22,4 +23,16 @@ describe('<EventList /> component', () => {
         EventListComponent.rerender(<EventList events={ allEvents } />);
         expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
     });
+})
+
+describe('<EventList /> integration', () => {
+    test('renders a list of 32 events wen the app is mounted and rendered', async() => {
+        const AppComponent = render(<App />);
+        const AppDOM = AppComponent.container.firstChild;
+        const EventListDOM = AppDOM.querySelector('#event-list');
+        await waitFor(() => {
+            const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+            expect(EventListItems.length).toBe(32)
+        })
+    })
 })
