55cc4c0220353eecae916c27f8725a437a5d6fca
Darkpfeffer <121576620+Darkpfeffer@users.noreply.github.com>
2023-07-09 17:00:43 +0200

    Unit tested Feature 2 Scenario 2 and 3

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,5 +1,6 @@
 //import
 import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 //import components
 import { Event } from "../components/Event/Event";
@@ -8,26 +9,71 @@ import { Event } from "../components/Event/Event";
 import { getEvents } from "../api";
 
 describe ('<Event /> component', () => {
+    let allEvents;
+    let EventComponent;
+    beforeEach(async() => {
+        allEvents = await getEvents();
+        EventComponent = render(<Event firstEvent={ allEvents[0]}/>)
+    })
+
     test('renders event summary', async () => {
-        const allEvents = await getEvents();
-        const EventComponent = render(<Event firstEvent={ allEvents[0]}/>)
         expect(EventComponent.queryByText(allEvents[0].summary)).toBeInTheDocument();
     })
 
     test('renders event start time', async() => {
-        const allEvents = await getEvents();
-        const EventComponent = render(<Event firstEvent={ allEvents[0]}/>)
         expect(EventComponent.queryByText(allEvents[0].start.dateTime)).toBeInTheDocument();
     })
 
+    test('renders event end time', async() => {
+        expect(EventComponent.queryByText(allEvents[0].end.dateTime)).toBeInTheDocument();
+    })
+
+    test('renders event time zone', async() => {
+        expect(EventComponent.queryByText(allEvents[0].start.timeZone)).toBeInTheDocument();
+    })
+
     test('renders event location', async() => {
-        const allEvents = await getEvents();
-        const EventComponent = render(<Event firstEvent={ allEvents[0]}/>)
         expect(EventComponent.queryByText(allEvents[0].location)).toBeInTheDocument();
     })
 
     test('renders event details button with the title (Show details)', () => {
-        const EventComponent = render(<Event />)
+        expect(EventComponent.queryByText(`Show details`)).toBeInTheDocument();
+    })
+
+    test('renders event details when the "show events" button is pressed by the user', async() => {
+        const user = userEvent.setup();
+        const showDetailsButton = EventComponent.queryByText(`Show details`);
+        await user.click(showDetailsButton)
+        expect(EventComponent.queryByText(`Hide details`)).toBeInTheDocument();
+    })
+
+    test('renders event details when "showDetails" is active', async() => {
+        const user = userEvent.setup();
+        const showDetailsButton = EventComponent.queryByText(`Show details`);
+        await user.click(showDetailsButton)
+        expect(EventComponent.queryByTestId('description')).toBeInTheDocument();
+    })
+
+    test('renders events html link when "showDetails" is active', async() => {
+        const user = userEvent.setup();
+        const showDetailsButton = EventComponent.queryByText(`Show details`);
+        await user.click(showDetailsButton)
+        expect(EventComponent.queryByText(allEvents[0].htmlLink)).toBeInTheDocument();
+    })
+
+    test('renders ICalendar ID when "showDetails" is active', async() => {
+        const user = userEvent.setup();
+        const showDetailsButton = EventComponent.queryByText(`Show details`);
+        await user.click(showDetailsButton)
+        expect(EventComponent.queryByText(allEvents[0].iCalUID)).toBeInTheDocument();
+    })
+
+    test('collapse event details when the "hide" button is pressed by the user', async() => {
+        const user = userEvent.setup();
+        const showDetailsButton = EventComponent.queryByText(`Show details`);
+        await user.click(showDetailsButton)
+        const hideDetailsButton = EventComponent.queryByText(`Hide details`);
+        await user.click(hideDetailsButton)
         expect(EventComponent.queryByText(`Show details`)).toBeInTheDocument();
     })
 })
--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,15 +1,39 @@
+//import
+import { useState } from "react"
+
 export const Event = ({ firstEvent }) => {
+    const [showDetails, setShowDetails] = useState(false)
+    const toggleDetails = (event) => {
+        event.preventDefault();
+        setShowDetails(!showDetails);
+    }
+
     return (
         <li>
-            {firstEvent ? (
+            {firstEvent && !showDetails ? (
                 <>
                     <p>{firstEvent.summary}</p>
                     <p>{firstEvent.start.dateTime}</p>
+                    <p>{firstEvent.end.dateTime}</p>
+                    <p>{firstEvent.start.timeZone}</p>
                     <p>{firstEvent.location}</p>
-                    
+                    <button onClick={toggleDetails}>Show details</button>
                 </>
             ) : null }
-            <button>Show details</button>
+
+            {firstEvent && showDetails ? (
+                <>
+                    <p>{firstEvent.summary}</p>
+                    <p>{firstEvent.start.dateTime}</p>
+                    <p>{firstEvent.end.dateTime}</p>
+                    <p>{firstEvent.start.timeZone}</p>
+                    <p>{firstEvent.location}</p>
+                    <p data-testid="description">{firstEvent.description}</p>
+                    <p>{firstEvent.htmlLink}</p>
+                    <p>{firstEvent.iCalUID}</p>
+                    <button onClick={toggleDetails}>Hide details</button>
+                </>
+            ) : null }
         </li>
     )
 }
