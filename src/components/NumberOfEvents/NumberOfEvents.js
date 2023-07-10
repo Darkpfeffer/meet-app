71e10529903417dde5f2be7b24192ecb7f35ab8a
Darkpfeffer <121576620+Darkpfeffer@users.noreply.github.com>
2023-07-10 13:43:42 +0200

    Updates

--- src/components/NumberOfEvents/NumberOfEvents.js
+++
@@ -1,9 +0,0 @@
-export const NumberOfEvents = () => {
-    return (
-        <div id="number-of-events">
-            <label>
-                <input role="textbox" defaultValue={32}></input>
-            </label> 
-        </div>
-    )
-}
