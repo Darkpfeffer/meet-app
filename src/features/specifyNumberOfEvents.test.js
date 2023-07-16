deebe6200bda7bfc65ee19b6ff0930e4d8865bde
Darkpfeffer <darkpfeffer97@gmail.com>
2023-07-16 16:30:30 +0200

    Acceptance tested Feature 3 Scenario 2

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -1,6 +1,7 @@
 //import
 import { loadFeature, defineFeature } from "jest-cucumber";
-import { render, within } from "@testing-library/react";
+import { render, within, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 //import components
 import { App } from "../App";
@@ -24,21 +25,38 @@ defineFeature(feature, test => {
         });
 
         then('the default number should be 32', () => {
-            expect(Number(NOEInput.value)).toBe(32)
+            expect(Number(NOEInput.value)).toBe(32);
         });
     });
 
     test('User can change the number of events they want to see', ({ given, when, then }) => {
+        let AppComponent;
+        let AppDOM;
         given('the main page is open', () => {
-
+            AppComponent = render(<App />)
+            AppDOM = AppComponent.container.firstChild;
         });
 
-        when('the user specifies the number of events visible', () => {
+        let NumberOfEventsDOM;
+        let NOEInput;
+        when('the user specifies the number of events visible', async() => {
+            const user = userEvent.setup();
+            NumberOfEventsDOM = AppDOM.querySelector('#number-of-events')
+            NOEInput = within(NumberOfEventsDOM).queryByRole('textbox');
 
+            await user.type(NOEInput, "{backspace}{backspace}10");
+
+            expect(NOEInput.value).toBe("10");
         });
 
-        then('the user should be able to see events equal to the given number at once', () => {
+        then('the user should be able to see events equal to the given number at once', async() => {
+            const EventListDOM = AppDOM.querySelector('#event-list')
+            let EventListItems;
 
+            await waitFor(() => {
+                EventListItems = within(EventListDOM).queryAllByRole('listitem');
+                expect(EventListItems.length).toBe(10);
+            })
         });
     });
 })
